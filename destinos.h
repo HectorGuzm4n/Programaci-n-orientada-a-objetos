2de473017fc34b238a6f18c68b92d7f1a9c01eaf
HectorGuzm4n <111297327+HectorGuzm4n@users.noreply.github.com>
2022-11-30 14:29:52 -0600

    Update destinos.h

--- destinos.h
+++ destinos.h
@@ -1,20 +1,35 @@
 class Destinos {
-	private:
+	private: //Atributos
 		int tiempo;
 		string lugar;
+		float kilometraje;
 		
-	public:
-		Destinos(int,string);
+	public: //Metodos
+		Destinos(int,string,float); //constructor
 		void horas();
 		void destino();
+		void km();
 
 };
 
-Destinos::Destinos(int _tiempo,string _lugar){
+class Costo : public Destinos{
+	private: //Atributos
+		int cantidad;
+	public: //Metodos
+		Costo(int,string,float,int); //Constructor de la clase Costo
+		void mostrarCosto();
+};
+//constructor de la clase padre
+Destinos::Destinos(int _tiempo,string _lugar,float _kilometraje){
 	tiempo=_tiempo;
 	lugar=_lugar;
+	kilometraje=_kilometraje;
 }
 
+Costo::Costo(int _tiempo,string _lugar,float _kilometraje,int _cantidad) : Destinos(_tiempo,_lugar,_kilometraje){
+
+	cantidad = _cantidad;
+}
 void Destinos::horas(){
 	cout<<"Tiempo: "<<tiempo<<" horas "<<endl;
 }
@@ -24,3 +39,16 @@ void Destinos::destino(){
 	
 
 } 
+
+void Destinos::km(){
+	cout<<kilometraje<<" Km"<<endl;
+}
+
+
+
+void Costo::mostrarCosto(){
+	destino();
+	horas();
+	km();
+	cout<<"Costo: $ "<<cantidad<<endl;
+}
