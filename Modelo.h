49ed022a905d4a07bdc655efd002f4159b15c225
HectorGuzm4n <111297327+HectorGuzm4n@users.noreply.github.com>
2022-12-02 11:45:05 -0600

    Update Modelo.h

--- Modelo.h
+++ Modelo.h
@@ -3,24 +3,27 @@ class Modelo {
 		string modelo2;
 		string modelo3;
 		string modelo4;
+		string modelo5;
 		
 	public: //metodos
 		Modelo(); //constructor
-		void setModelo(string,string,string);
+		void setModelo(string,string,string,string);
 		//Getters
 		string getModelomodelo2();
 		string getModelomodelo3();
 		string getModelomodelo4();
+		string getModelomodelo5();
 
 };
 
 Modelo::Modelo(){
 }
 //setters
-void Modelo::setModelo(string _modelo2,string _modelo3,string _modelo4){
+void Modelo::setModelo(string _modelo2,string _modelo3,string _modelo4,string _modelo5){
 	modelo2 = _modelo2;
 	modelo3 = _modelo3;
 	modelo4 = _modelo4;
+	modelo5 = _modelo5;
 }	
 
 string Modelo::getModelomodelo2(){
@@ -33,4 +36,8 @@ string Modelo::getModelomodelo3(){
 
 string Modelo::getModelomodelo4(){
 	return modelo4;
+}
+
+string Modelo::getModelomodelo5(){
+	return modelo5;
 }
